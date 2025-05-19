869ad05f427292e069343012c04faa78fcae9e0d
Cyril Joseph <ccyyrriill83@gmail.com>
2025-05-19 08:55:05 -0300

    added login and services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { UserService } from './core/services/user.service';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './common/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'service-provider-app';
+  isUserLoggedIn = false;
+  private userSubscription!: Subscription;
+
+  constructor(private userService: UserService
+  ) { }
+
+  ngOnInit(): void {
+
+    this.isUserLoggedIn = this.userService.isLoggedIn();
+
+    this.userSubscription = this.userService
+      .watchUser()
+      .subscribe(userLoggedIn => {
+        this.isUserLoggedIn = userLoggedIn
+      });
+  }
+
+  ngOnDestroy(): void {
+    if (this.userSubscription) {
+      this.userSubscription.unsubscribe();
+    }
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './auth/auth.guard';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    //  { path: 'usersettings', component: UserSettingsComponent },
+    { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+    //   { path: 'service-provider/:id', component: EditServiceProviderComponent, canActivate: [AuthGuard] },
+    //  { path: 'add-service-provider', component: AddServiceProviderComponent, canActivate: [AuthGuard] },
+    { path: '404', component: NotFoundComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '**', redirectTo: '/404' }
+];
